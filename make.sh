4290b07de71c84a37308f9442519a7fecb611501
jesse <mr.bossman075@gmail.com>
2021-01-09 02:40:23 -0500

    stuff

--- make.sh
+++ make.sh
@@ -32,6 +32,7 @@ make clean
 
 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- make linux_defconfig
 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- make -j64 deb-pkg LOCALVERSION=-custom
+ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- make -j64
 cd ..
 
 
